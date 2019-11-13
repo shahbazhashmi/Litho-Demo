7a4a633c6216a4484474ef1f1386a8eaf0db4b20
Shahbaz Hashmi <shahbaz@DEV-386.local>
2019-11-13 18:46:14 +0530

    modified list

--- app/src/main/java/matrixsystems/lithodemo/layouts/ListItemSpec.kt
+++ app/src/main/java/matrixsystems/lithodemo/layouts/ListItemSpec.kt
@@ -10,6 +10,9 @@ import com.facebook.litho.annotations.OnCreateLayout
 import com.facebook.litho.widget.Text
 
 import com.facebook.yoga.YogaEdge.ALL
+import com.facebook.litho.annotations.Prop
+
+
 
 /**
  * Created by Shahbaz Hashmi on 2019-11-13.
@@ -17,7 +20,7 @@ import com.facebook.yoga.YogaEdge.ALL
 @LayoutSpec
 object ListItemSpec {
 
-    @JvmStatic
+   /* @JvmStatic
     @OnCreateLayout
     internal fun onCreateLayout(c: ComponentContext): Component {
 
@@ -32,6 +35,32 @@ object ListItemSpec {
             .child(
                 Text.create(c)
                     .text("Litho tutorial")
+                    .textSizeSp(20f)
+            )
+            .build()
+    }*/
+
+
+    @JvmStatic
+    @OnCreateLayout
+    fun onCreateLayout(
+        c: ComponentContext,
+        @Prop color: Int,
+        @Prop title: String,
+        @Prop subtitle: String
+    ): Component {
+
+        return Column.create(c)
+            .paddingDip(ALL, 16f)
+            .backgroundColor(color)
+            .child(
+                Text.create(c)
+                    .text(title)
+                    .textSizeSp(40f)
+            )
+            .child(
+                Text.create(c)
+                    .text(subtitle)
                     .textSizeSp(20f)
             )
             .build()
--- app/src/main/java/matrixsystems/lithodemo/layouts/ListSectionSpec.kt
+++ app/src/main/java/matrixsystems/lithodemo/layouts/ListSectionSpec.kt
@@ -5,6 +5,9 @@ import com.facebook.litho.sections.SectionContext
 import com.facebook.litho.sections.annotations.GroupSectionSpec
 import com.facebook.litho.sections.annotations.OnCreateChildren
 import com.facebook.litho.sections.common.SingleComponentSection
+import android.graphics.Color
+
+
 
 /**
  * Created by Shahbaz Hashmi on 2019-11-13.
@@ -19,9 +22,18 @@ object ListSectionSpec {
 
         for (i in 0..31) {
             builder.child(
+                /*SingleComponentSection.create(c)
+                    .key(i.toString())
+                    .component(ListItem.create(c).build())*/
                 SingleComponentSection.create(c)
                     .key(i.toString())
-                    .component(ListItem.create(c).build())
+                    .component(
+                        ListItem.create(c)
+                            .color(if (i % 2 == 0) Color.WHITE else Color.LTGRAY)
+                            .title("$i. Hello, world!")
+                            .subtitle("Litho tutorial")
+                            .build()
+                    )
             )
         }
         return builder.build()
