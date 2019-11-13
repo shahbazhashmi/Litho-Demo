8b21e9edd37315a806a5677a0dd4020f364d94fc
Shahbaz Hashmi <shahbaz@DEV-386.local>
2019-11-13 18:56:38 +0530

    added MovieComponentSpec

---
+++ app/src/main/java/matrixsystems/lithodemo/layouts/MovieComponentSpec.kt
@@ -0,0 +1,45 @@
+package matrixsystems.lithodemo.layouts
+
+import android.graphics.Color
+
+import com.facebook.litho.Column
+import com.facebook.litho.Component
+import com.facebook.litho.ComponentContext
+import com.facebook.litho.annotations.LayoutSpec
+import com.facebook.litho.annotations.OnCreateLayout
+import com.facebook.litho.annotations.Prop
+import com.facebook.litho.widget.Text
+
+import com.facebook.yoga.YogaEdge.ALL
+
+/**
+ * Created by Shahbaz Hashmi on 2019-11-13.
+ */
+@LayoutSpec
+object MovieComponentSpec {
+
+    @JvmStatic
+    @OnCreateLayout
+    internal fun onCreateLayout(
+        c: ComponentContext,
+        @Prop title: String,
+        @Prop imageUrl: String
+    ): Component {
+
+        return Column.create(c)
+            .paddingDip(ALL, 16f)
+            .backgroundColor(Color.WHITE)
+            .child(
+                Text.create(c)
+                    .text("Hello world")
+                    .textSizeSp(40f)
+            )
+            .child(
+                Text.create(c)
+                    .text("Litho tutorial")
+                    .textSizeSp(20f)
+            )
+            .build()
+    }
+
+}
