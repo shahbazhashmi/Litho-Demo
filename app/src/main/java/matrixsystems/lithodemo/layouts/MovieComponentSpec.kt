0c0a49c642610f6901115a87443ebefd2eba963a
Shahbaz Hashmi <shahbaz@DEV-386.local>
2019-12-03 13:43:17 +0530

    added placeholder

--- app/src/main/java/matrixsystems/lithodemo/layouts/MovieComponentSpec.kt
+++ app/src/main/java/matrixsystems/lithodemo/layouts/MovieComponentSpec.kt
@@ -14,6 +14,7 @@ import com.facebook.litho.widget.Text
 
 import com.facebook.yoga.YogaEdge.ALL
 import com.facebook.drawee.backends.pipeline.Fresco
+import com.facebook.drawee.drawable.ScalingUtils
 import com.facebook.drawee.interfaces.DraweeController
 import matrixsystems.lithodemo.R
 
@@ -38,9 +39,13 @@ object MovieComponentSpec {
     ): Component {
 
         return Column.create(c)
+            .paddingDip(ALL, 8f)
             .backgroundColor(Color.BLACK)
             .child(
-                FrescoImage.create(c).controller(getImageController(imageUrl)).imageAspectRatio(0.66666666666667f)
+                FrescoImage.create(c).controller(getImageController(imageUrl))
+                    .placeholderImageRes(R.drawable.movie_placeholder)
+                    .placeholderImageScaleType(ScalingUtils.ScaleType.CENTER_CROP)
+                    .imageAspectRatio(0.66666666666667f)
             )
             /*.child(
                 Text.create(c)
